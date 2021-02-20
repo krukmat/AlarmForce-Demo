7ed49985bc527d0431f2c9c313a6d100d9063f35
Matias Kruk <kruk.matias@gmail.com>
2021-02-20 10:22:18 +0100

    forever.py in python3

--- python/forever.py
+++ python/forever.py
@@ -1,9 +1,9 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 from subprocess import Popen
 import sys
 
-filename = 'salesforce.py'
+filename = './salesforce.py'
 while True:
     print("\nStarting " + filename)
-    p = Popen("python " + filename, shell=True)
+    p = Popen("python3 " + filename, shell=True)
     p.wait()
